f1af3d8ffc1a6fbd92b4d4ba8484127f399c7db9
zswxp32 <zswxp32@163.com>
2020-11-04 22:49:21 +0800

    feat: add feed back entry

--- src/component/Version/index.tsx
+++ src/component/Version/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './index.module.scss';
 
 export function Version({ version }) {
+  const feecBack = useCallback(() => {
+    window.open('https://github.com/zswxp32/mou-fund/issues');
+  }, []);
+
   return <div className={styles.version}>
     <span>大牛基金助手</span>
     <span>Version {version}</span>
-    <span className={styles.feedback}>反馈</span>
+    <span className={styles.feedback} onClick={feecBack}>反馈</span>
   </div>;
 }
